7fe2d930d71941d0e369c438409b7348b5c3fd52
Norman Alié <mail@normanalie.fr>
2022-03-13 14:52:34 +0100

    Add login form frontend

--- app/user/user.py
+++ app/user/user.py
@@ -3,7 +3,10 @@ from flask import render_template
 from app import db
 from app.user import bp
 
+from app.user.forms import LoginForm
+
 
 @bp.route('/login/')
 def login():
-    return render_template("user/login.html")
+    form = LoginForm()
+    return render_template("user/login.html", form=form)

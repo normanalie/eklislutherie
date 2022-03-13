82e2d293fbc757c2b32b69b72f9632e884f49250
Norman Alié <mail@normanalie.fr>
2022-03-13 15:09:43 +0100

    Add login form logic

--- app/user/user.py
+++ app/user/user.py
@@ -1,12 +1,31 @@
-from flask import render_template
+from flask import redirect, render_template, url_for, request
+from flask_login import current_user, login_user
+from werkzeug.urls import url_parse
 
 from app import db
+from app.models import User
 from app.user import bp
 
 from app.user.forms import LoginForm
 
 
-@bp.route('/login/')
+@bp.route('/login/', methods=['GET', 'POST'])
 def login():
+    errors = []
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+
     form = LoginForm()
-    return render_template("user/login.html", form=form)
+    if form.validate_on_submit():  # POST processing
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            errors.append("Pas d'utilisateurs avec cet email")
+        elif not user.check_password(form.password.data):
+            errors.append("Mot de passe incorrect")
+        else:
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':  # Check if there is a next_page and if the next_page is a relative path
+                next_page = url_for('main.index')
+            return redirect(next_page)
+    return render_template('user/login.html', form=form, errors=errors)

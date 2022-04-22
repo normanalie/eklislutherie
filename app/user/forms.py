7deed3c79d14ceb5424cbab6f51255b54ca5c41a
Norman Alié <mail@normanalie.fr>
2022-04-22 10:50:38 +0200

    Add submit button to achievements edit

--- app/user/forms.py
+++ app/user/forms.py
@@ -11,7 +11,8 @@ class LoginForm(FlaskForm):
 
 class ArticleForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
-    cover_img = FileField("Cover Image", validators=[DataRequired(), regexp(u'[^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$')])
+    cover_img = FileField("Cover Image", validators=[regexp(u'[^\\s]+(\\.(?i)(jpe?g|png|gif|bmp))$')])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     content = TextAreaField("Content", validators=[DataRequired()])
     tags = SelectMultipleField("Tags")
+    submit = SubmitField('Save')

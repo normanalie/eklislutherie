27e3368fb6da242adc3a400cf07a295c2f47b2ed
Norman Alié <mail@normanalie.fr>
2022-05-24 11:08:02 +0200

    Force conf

--- app/config.py
+++ app/config.py
@@ -9,11 +9,11 @@ To start app:
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SERVER_NAME=os.environ.get('SERVER_NAME')
 APPLICATION_ROOT=os.environ.get('APPLICATION_ROOT') or '/'
 PREFERRED_URL_SCHEME="https"
 
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'yTnSN65a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False

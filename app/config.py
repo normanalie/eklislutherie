db96b015c1da9d090e1c4dea25a6a91bb054d850
Norman Alié <mail@normanalie.fr>
2022-05-24 11:02:46 +0200

    More flexible config

--- app/config.py
+++ app/config.py
@@ -9,6 +9,9 @@ To start app:
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+APPLICATION_ROOT=os.environ.get('APPLICATION_ROOT') or '/'
+PREFERRED_URL_SCHEME="https"
+
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'yTnSN65a'
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
